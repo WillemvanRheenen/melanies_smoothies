868f3144c97ac02167d4f0d249ba65f7b110c259
WillemvanRheenen <77116242+WillemvanRheenen@users.noreply.github.com>
2025-03-14 15:27:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition information')
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     # st.write(my_insert_stmt)
