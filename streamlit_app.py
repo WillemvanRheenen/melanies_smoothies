ef2e41586ba756512eecabccd65523769c3ad28e
WillemvanRheenen <77116242+WillemvanRheenen@users.noreply.github.com>
2025-03-14 14:46:08 +0100

    Update streamlit_app.py
    extra collumn

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,9 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 # st.write("Your favorite fruit is:", option)
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')), select(col('SEARCH_ON'))
 st.dataframe(data = my_dataframe, use_container_width=True)
-
+st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',
                                  my_dataframe)
 if ingredients_list:
